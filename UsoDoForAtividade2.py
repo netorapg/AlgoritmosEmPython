614cc33455988a22622be73e97d2bd2d02199b7e
Renato Augusto <107155217+Argustor@users.noreply.github.com>
2023-03-13 09:34:48 -0300

    Delete UsoDoForAtividade2.py

--- UsoDoForAtividade2.py
+++
@@ -1,7 +0,0 @@
-#2) Faça um programa que leia 5 números e informe apenas o maior número.
-list = []
-
-for n in range(0, 5):
-    list.append(int(input("Digite o número:")))
-
-print ("Maior número da lista: ", max(list))
